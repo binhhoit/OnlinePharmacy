d76813ebd90e8e6fe29c2bf6468aeef08d54752c
KD_3004\ASUS_K46CM <hobinhit@gmail.com>
2017-06-21 14:23:14 +0700

    check style v4

--- project5/app/src/main/java/com/example/thanh/OnlinePharmacy/service/network/RetrofitInterface.java
+++ project5/app/src/main/java/com/example/thanh/OnlinePharmacy/service/network/RetrofitInterface.java
@@ -24,33 +24,43 @@ import rx.Observable;
 public interface RetrofitInterface {
     @POST("users")
     Observable<Response> register(@Body User user);
+
     //  ứng với router.post('/authenticate', (req, res)
     @POST("authenticate")
     Observable<Response> login();
+
     //  router.get('/users/:id', (req,res)
     @GET("users/{email}")
     Observable<User> getProfile(@Path("email") String email);
+
     //  router.put('/users/:id'
     @PUT("users/{email}")
     Observable<Response> changePassword(@Path("email") String email, @Body User user);
+
     //  router.post('/users/:id/password' dùng chung vs bên dưới
     @POST("users/{email}/password")
     Observable<Response> resetPasswordInit(@Path("email") String email);
+
     //  router.post('/users/:id/password'
     @POST("users/{email}/password")
     Observable<Response> resetPasswordFinish(@Path("email") String email, @Body User user);
+
     //  router.get('/Prescription'
     @GET("Prescription/{id}")
     Call<List<Prescription>> getPrescription(@Path("id") String id);
+
     //  router.post('/Prescription'/confirmList/
     @GET("Prescription/{id}/true")
     Call<List<Prescription>> getPrescriptionConfirm(@Path("id") String id);
+
     //  router.post('/Prescription'
     @POST("Prescription")
     Call<ResponseStatus> postPrescription(@Body Prescription Prescription);
+
     //  router.get('/prescriptionlist'
     @POST("1pay")
     Call<ResponseStatus> postPayCard(@Body PayCard payCard);
+
     @POST("Prescription/photo")
     Call<ResponseStatus> postPhotoPrescription(@Body PhotoPrescription PhotoPrescription);
 }
